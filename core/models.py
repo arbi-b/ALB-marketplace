97f8ae3ea5139bbb6346868880117ef4bb058421
xugv25 <arbi.backa@gmail.com>
2023-07-19 18:05:15 +0200

    modified:   core/__pycache__/models.cpython-311.pyc 	modified:   core/__pycache__/views.cpython-311.pyc 	new file:   core/migrations/0020_post_price.py 	new file:   core/migrations/0021_post_photos2_post_photos3_post_photos4_post_photos5_and_more.py 	new file:   core/migrations/0022_alter_post_price.py 	new file:   core/migrations/__pycache__/0020_post_price.cpython-311.pyc 	new file:   core/migrations/__pycache__/0021_post_photos2_post_photos3_post_photos4_post_photos5_and_more.cpython-311.pyc 	new file:   core/migrations/__pycache__/0022_alter_post_price.cpython-311.pyc 	modified:   core/models.py 	modified:   core/templates/core/about.html 	modified:   core/templates/core/base.html 	modified:   core/templates/core/categories.html 	modified:   core/templates/core/home.html 	modified:   core/templates/core/post_detail.html 	modified:   core/views.py 	modified:   db.sqlite3 	modified:   marketplace/__pycache__/settings.cpython-311.pyc 	modified:   marketplace/settings.py 	new file:   media/defaultpic.jpg 	new file:   media/icons/cars.png 	new file:   media/icons/loupe.png 	new file:   media/images/1.jpg 	new file:   media/images/2.jpg 	new file:   media/images/3.jpg 	new file:   media/images/4.jpg 	new file:   media/images/WhatsApp_Image_2023-07-04_at_6.21.19_PM.jpeg 	new file:   media/images/WhatsApp_Image_2023-07-04_at_7.33.59_PM.jpeg 	new file:   media/images/bmw-328-benzin-2014-full-opsion.jpeg 	new file:   media/images/bmw-328-benzin-2014-full-opsion_1.jpeg 	new file:   media/images/bmw-328-benzin-2014-full-opsion_2.jpeg 	new file:   media/images/bmw-328-benzin-2014-full-opsion_3.jpeg 	new file:   media/images/bmw_GpAIGkl.jpg 	new file:   media/images/cls-320-naft-2007-super-gjendje.jpeg 	new file:   media/images/cls-320-naft-2007-super-gjendje_1.jpeg 	new file:   media/images/cls-320-naft-2007-super-gjendje_2.jpeg 	new file:   media/images/cls-320-naft-2007-super-gjendje_qqCzsnv.jpeg 	new file:   media/images/download_a8i5AzW.jpg 	new file:   media/images/foto_LaoxPKZ.jpg 	new file:   media/images/honda1.jpg 	new file:   media/images/honda2.jpg 	new file:   media/images/honda3.jpg 	new file:   media/images/honda3_o4NdYTH.jpg 	new file:   media/images/mustang.jpg 	new file:   media/images/mustang2.jpg 	new file:   media/images/phone.jpg 	new file:   media/images/phone2.jpg 	new file:   media/images/shepi2.jpg 	new file:   media/images/shtepi3.jpg 	new file:   media/images/shtepi4.jpg 	new file:   media/images/shtepi_1.jpg 	new file:   media/images/shtepii.jpg 	new file:   media/profile_pics/bmw-328-benzin-2014-full-opsion.jpeg 	new file:   media/profile_pics/bmw-328-benzin-2014-full-opsion_3.jpeg 	new file:   media/profile_pics/honda3.jpg 	modified:   users/__pycache__/forms.cpython-311.pyc 	modified:   users/__pycache__/models.cpython-311.pyc 	modified:   users/__pycache__/views.cpython-311.pyc 	modified:   users/forms.py 	new file:   users/migrations/0005_profile_phone_number.py 	new file:   users/migrations/0006_remove_profile_phone_number.py 	new file:   users/migrations/0007_profile_phone_number.py 	new file:   users/migrations/0008_remove_profile_phone_number.py 	new file:   users/migrations/0009_profile_phone_number.py 	new file:   users/migrations/0010_alter_profile_phone_number.py 	new file:   users/migrations/__pycache__/0005_profile_phone_number.cpython-311.pyc 	new file:   users/migrations/__pycache__/0006_remove_profile_phone_number.cpython-311.pyc 	new file:   users/migrations/__pycache__/0007_profile_phone_number.cpython-311.pyc 	new file:   users/migrations/__pycache__/0008_remove_profile_phone_number.cpython-311.pyc 	new file:   users/migrations/__pycache__/0009_profile_phone_number.cpython-311.pyc 	new file:   users/migrations/__pycache__/0010_alter_profile_phone_number.cpython-311.pyc 	modified:   users/models.py 	modified:   users/templates/users/profile.html 	modified:   users/templates/users/register.html

--- core/models.py
+++ core/models.py
@@ -15,8 +15,18 @@ class Post(models.Model):
     content = models.TextField(default='')
     date_posted = models.DateTimeField(default=timezone.now)
     author = models.ForeignKey(User, on_delete=models.CASCADE, default=User.objects.first().id)
-    photos = models.ImageField(null=True, blank=True, upload_to='images')
+    photos = models.ImageField(default="defaultpic.jpg", upload_to='images')
+    photos2 = models.ImageField(null=True, blank=True, upload_to='images')
+    photos3 = models.ImageField(null=True, blank=True, upload_to='images')
+    photos4 = models.ImageField(null=True, blank=True, upload_to='images')
+    photos5 = models.ImageField(null=True, blank=True, upload_to='images')
+    photos6 = models.ImageField(null=True, blank=True, upload_to='images')
+    photos7 = models.ImageField(null=True, blank=True, upload_to='images')
+    photos8 = models.ImageField(null=True, blank=True, upload_to='images')
+    photos9 = models.ImageField(null=True, blank=True, upload_to='images')
     category = models.ForeignKey(Category, on_delete=models.CASCADE, default='1')
+    price= models.DecimalField(max_digits=10, decimal_places=0, default='0')
+
 
 
     
@@ -26,32 +36,3 @@ class Post(models.Model):
 
     def get_absolute_url(self):
         return reverse('post-detail', kwargs={'pk' : self.pk})
-    
-
-# class Car(models.Model):
-#     post = models.OneToOneField(Post, on_delete=models.CASCADE, primary_key=True)
-#     model = models.CharField(max_length=50)
-#     year = models.IntegerField()
-#     price = models.DecimalField(max_digits=10, decimal_places=2)
-    
-#     def __str__(self):
-#         return self.model
-    
-
-# class Job(models.Model):
-#     post = models.OneToOneField(Post, on_delete=models.CASCADE, primary_key=True)
-#     title = models.CharField(max_length=100)
-#     location = models.CharField(max_length=100)
-#     salary = models.DecimalField(max_digits=10, decimal_places=2)
-    
-#     def __str__(self):
-#         return self.title
-    
-
-# class Property(models.Model):
-#     post = models.OneToOneField(Post, on_delete=models.CASCADE, primary_key=True)
-#     address = models.CharField(max_length=100)
-#     price = models.DecimalField(max_digits=10, decimal_places=2)
-    
-#     def __str__(self):
-#         return self.address    
--- core/views.py
+++ core/views.py
@@ -1,9 +1,10 @@
+from typing import Any, Dict
 from django.shortcuts import render
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 from django.views.generic.detail import DetailView
 from .models import Post, Category
-
+from django.db.models import Q
 
 def home(request):
     context = {
@@ -16,7 +17,9 @@ def home(request):
 def search(request):
     if request.method == "POST":
         searched = request.POST['searched']
-        posts = Post.objects.filter(title__contains=searched)
+        posts = Post.objects.filter(
+            Q(title__contains=searched) | Q(content__contains=searched)
+        )
         return render(request, 'core/search.html', {'searched':searched, 'posts':posts})
     else:
         return render(request, 'core/search.html', {})
@@ -37,6 +40,12 @@ class PostListView(ListView):
     context_object_name = 'posts'
     ordering = ['-date_posted']
 
+    def get_context_data(self, *args, **kwargs):
+        cat_menu = Category.objects.all()
+        context = super(PostListView, self).get_context_data(*args, **kwargs)
+        context["cat_menu"] = cat_menu
+        return context
+
 
 
 class PostDetailView(DetailView):
@@ -45,7 +54,7 @@ class PostDetailView(DetailView):
 
 class PostCreateView(LoginRequiredMixin, CreateView):
     model = Post
-    fields = ['category', 'title', 'content', 'photos']
+    fields = ['category', 'title', 'content', 'photos', 'photos2','photos3','photos4','photos5','photos6','photos7','photos8','photos9', 'price']
 
     def form_valid(self, form):
         form.instance.author = self.request.user
@@ -56,11 +65,12 @@ class PostCreateView(LoginRequiredMixin, CreateView):
             form = PostCreateView(request.POST, request.FILES)
             if form.is_valid():
                 form.save()
+            
         
 
 class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = Post
-    fields = ['category', 'title', 'content', 'photos']
+    fields = ['category', 'title', 'content', 'photos', 'photos2','photos3','photos4','photos5','photos6','photos7','photos8','photos9', 'price']
     
     def form_valid(self, form):
         form.instance.author = self.request.user
